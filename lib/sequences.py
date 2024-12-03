99276471efa3c5611be1d529fa383fb34695b3ee
--add <168851515+nelly2040@users.noreply.github.com>
2024-12-03 15:52:38 +0300

    commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    if length <= 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+    elif length == 2:
+        print([0, 1])
+        return
+
+    fibonacci_sequence = [0, 1]
+    
+    for i in range(2, length):
+        next_value = fibonacci_sequence[-1] + fibonacci_sequence[-2]
+        fibonacci_sequence.append(next_value)
+    
+    print(fibonacci_sequence)
+
+
     pass
